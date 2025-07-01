47a658c0318a937f8a7c58992775d641cf22dd73
Stanislav <ssovelich@gmail.com>
2025-07-01 20:39:11 +0300

    added contact page

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -1,9 +1,51 @@
-import React from 'react'
+import styles from "@/styles/pages/ContactPage.module.css";
+import { FaPhoneAlt } from "react-icons/fa";
+import { FaLocationDot } from "react-icons/fa6";
+import { MdOutlineEmail } from "react-icons/md";
 
 const ContactPage = () => {
   return (
-    <div>ContactPage</div>
-  )
-}
+    <>
+      <div className={styles.bg}>
+        <h1 className={`container ${styles.title}`}>Contact</h1>
+      </div>
+      <div className={`container ${styles.contactContent}`}>
+        <ul className={styles.contactList}>
+          <li className={styles.contactItem}>
+            <div className={styles.iconWrap}>
+              <FaLocationDot size={30} />
+            </div>
+            <div className={styles.contactDetails}>
+              <h3 className={styles.titleItem}>Location</h3>
+              <p className={styles.text}>
+                Buttonwood, California. <span>Rosemead, CA 91770</span>
+              </p>
+            </div>
+          </li>
+          <li className={styles.contactItem}>
+            <div className={styles.iconWrap}>
+              <FaPhoneAlt size={30} />
+            </div>
+            <div className={styles.contactDetails}>
+              <h3 className={styles.titleItem}>Phone</h3>
+              <p className={styles.text}>(90) 277 808 2566</p>
+              <p className={styles.text}>(78) 267 202 2578</p>
+            </div>
+          </li>
+          <li className={styles.contactItem}>
+            <div className={styles.iconWrap}>
+              <MdOutlineEmail size={30} />
+            </div>
+            <div className={styles.contactDetails}>
+              <h3 className={styles.titleItem}>Email</h3>
+              <p className={styles.text}>zacson@gmail.com</p>
+              <p className={styles.text}>contact@zacson.com</p>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </>
+  );
+};
 
-export default ContactPage
+export default ContactPage;
