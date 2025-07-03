5a0ff6281542fe379c9cf5980ca9e19a345a63c0
Stanislav <ssovelich@gmail.com>
2025-07-03 14:45:01 +0300

    added redirect not found

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -1,6 +1,20 @@
+"use client";
+
 import styles from "./not-found.module.css";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 const NotFound = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      router.push("/");
+    }, 3000);
+
+    return () => clearTimeout(timeout);
+  }, [router]);
+
   return (
     <>
       <div className={styles.bg}>
