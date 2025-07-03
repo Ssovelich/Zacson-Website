948aa110c5c269b249a8095c894e63bdbdb82b5e
Stanislav <ssovelich@gmail.com>
2025-07-03 13:11:21 +0300

    added not found page

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import styles from "./not-found.module.css";
 
 const NotFound = () => {
   return (
-    <div>Not Found Page</div>
-  )
-}
+    <>
+      <div className={styles.bg}>
+        <h1 className={`container ${styles.title}`}>404 – Page Not Found</h1>
+      </div>
+      <div className={`container ${styles.notFoundContent}`}>
+        <p className={styles.text}>
+          Oops! The page you’re looking for doesn’t exist or has been moved.
+        </p>
+      </div>
+    </>
+  );
+};
 
-export default NotFound
+export default NotFound;
