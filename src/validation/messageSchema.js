c87170d35090f1f2221c6db11315afcdf02847fc
Stanislav <ssovelich@gmail.com>
2025-07-07 17:14:19 +0300

    fix validation schema

--- src/validation/messageSchema.js
+++ src/validation/messageSchema.js
@@ -3,19 +3,19 @@ import { z } from "zod";
 export const messageSchema = z.object({
   name: z
     .string()
-    .min(3, "The name must contain at least 3 characters.")
-    .max(20, "The name cannot exceed 20 characters.")
-    .nonempty("Name is required."),
+    .min(3, "The name must contain at least 3 characters")
+    .max(20, "The name cannot exceed 20 characters")
+    .nonempty("Name is required"),
 
   email: z
     .string()
-    .nonempty("Email is required.")
-    .email("Incorrect email format."),
+    .nonempty("Email is required")
+    .email("Incorrect email format"),
 
   phone: z
     .string()
-    .nonempty("Phone number is required.")
-    .regex(/^\+?\d{9,12}$/, "Invalid phone number format."),
+    .nonempty("Phone number is required")
+    .regex(/^\+?\d{9,12}$/, "Invalid phone number format"),
 
   message: z.string().optional(),
 });
